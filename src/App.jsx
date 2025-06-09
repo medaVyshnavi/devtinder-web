681766bbdaf656a81a15d15a236097a857ceb860
medaVyshnavi <vyshnavivenkatesh@gmail.com>
2025-06-09 22:04:58 +0900

    layout and router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import Navbar from "./components/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./components/Login"
+import Body from "./Body";
+import Profile from "./components/Profile"
+import HomePage from "./components/HomePage"
 
 function App() {
   return (
     <>
-      <Navbar/>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/login" element={ <Login/>} />
+          <Route path="/feed" element={<Body />} >
+            <Route path="profile" element={<Profile/>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
